1ddd8e96b6917684f9508f4de41fcfad5c913803
JacksonWeudes <weudes2024jackson@gmail.com>
2025-10-24 15:34:35 -0300

    estrutura finalizada.

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ function toggleMenu()
     }) 
 }
 
+function headerTransition()
+{
+    const header = document.querySelector(".header")
+
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 10){
+            header.classList.add("active")
+        }
+        else{
+            header.classList.remove("active")
+        }
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     toggleMenu()
+    headerTransition()
 })
