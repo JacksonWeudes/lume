6a44ee6d2f2a3c665865a14bd9892a27cba08acf
JacksonWeudes <weudes2024jackson@gmail.com>
2025-10-29 15:29:23 -0300

    reajustes no script

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const sideMenu  = document.querySelector(".side-menu")
+const menuButton = document.querySelector(".mobile-icon")
+const header = document.querySelector(".header")
+
+
 function toggleMenu()
 {
-    const sideMenu   = document.querySelector(".side-menu")
-    const menuButton = document.querySelector(".mobile-icon")
     const header = document.querySelector(".header")
+    const overlay = document.querySelector(".overlay")
     
     menuButton.addEventListener("click", () => {
         menuButton.classList.toggle("active")
         sideMenu.classList.toggle("active")
+        overlay.classList.toggle("active")
+        document.body.classList.toggle("active")
 
         if(window.scrollY == 0){
             header.classList.toggle("active")
@@ -14,10 +20,24 @@ function toggleMenu()
     }) 
 }
 
+function autoCloseSideMenu()
+{
+    const sideMenu  = document.querySelector(".side-menu")
+    const sideLinks = document.querySelectorAll(".side-link")
+    const menuButton = document.querySelector(".mobile-icon")
+
+    sideLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            if(sideMenu.classList.contains("active")){
+                sideMenu.classList.remove("active")
+                menuButton.classList.remove("active")
+            }
+        })
+    })
+}
+
 function headerTransition()
 {
-    const header = document.querySelector(".header")
-
     window.addEventListener("scroll", () => {
         if(window.scrollY > 10){
             header.classList.add("active")
@@ -30,5 +50,6 @@ function headerTransition()
 
 document.addEventListener("DOMContentLoaded", () => {
     toggleMenu()
+    autoCloseSideMenu()
     headerTransition()
 })
